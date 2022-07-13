6250f91ed61c13f1cbfce3efa36e361309303905
Philipp Wissmann <philipp.wissmann@campus.lmu.de>
2022-07-13 14:00:50 +0200

    added more utilities from apache libraries

--- src/main/java/net/finmath/lasso/Utilities.java
+++ src/main/java/net/finmath/lasso/Utilities.java
@@ -4,13 +4,59 @@
 package net.finmath.lasso;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 
 /**
  * @author Christian Fries
  */
 public class Utilities {
 
+	/**
+	 * Addition of two matrices.
+	 *
+	 * @param left The matrix A
+	 * @param right The matrix B
+	 * @return sum The matrix sum of A+B (if suitable)
+	 */
+	public static double[][] add(final double[][] left, final double[][] right) {
+		return (new Array2DRowRealMatrix(left)).add(new Array2DRowRealMatrix(right)).getData();
+	}
+	
+	/**
+	 * Addition of two vectors.
+	 *
+	 * @param left The vector u
+	 * @param right The vector v
+	 * @return sum The vector sum of u+v (if suitable)
+	 */
+	public static double[] add(final double[] left, final double[] right) {
+		return (new ArrayRealVector(left)).add(new ArrayRealVector(right)).toArray();
+	}
+	
+	/**
+	 * Subtraction of two matrices.
+	 *
+	 * @param left The matrix A.
+	 * @param right The matrix B
+	 * @return difference The matrix difference of A-B (if suitable)
+	 */
+	public static double[][] subtract(final double[][] left, final double[][] right) {
+		return (new Array2DRowRealMatrix(left)).subtract(new Array2DRowRealMatrix(right)).getData();
+	}
+	
+	/**
+	 * Subtraction of two vectors.
+	 *
+	 * @param left The vector u
+	 * @param right The vector v
+	 * @return difference The vector difference of u-v (if suitable)
+	 */
+	public static double[] subtract(final double[] left, final double[] right) {
+		return (new ArrayRealVector(left)).subtract(new ArrayRealVector(right)).toArray();
+	}
+	
 	/**
 	 * Multiplication of two matrices.
 	 *
@@ -33,6 +79,35 @@ public class Utilities {
 		return (new Array2DRowRealMatrix(matrix)).operate(vector);
 	}
 	
+	/**
+	 * Multiplication of two vectors
+	 *
+	 * @param left The vector u
+	 * @param right The vector v
+	 * @return product The dot product of u*v (if suitable)
+	 */
+	public static double mult(final double[] left, final double[] right) {
+		return (new ArrayRealVector(left)).dotProduct(new ArrayRealVector(right));
+	}
+	
+	/**
+	 * Mapped vector
+	 * 
+	 * @param vector The vector u
+	 * @param function The univariate function f
+	 * @return mappedValue The mapped vector f(u)
+	 */
+	public static double[] map(final double[] vector, final UnivariateFunction function) {
+		return (new ArrayRealVector(vector)).map(function).toArray();
+	}
+	
+	/**
+	 * 
+	 * @param array
+	 * @param startIndexInclusive
+	 * @param endIndexExclusive
+	 * @return subArray 
+	 */
 	public static double[] subArray(final double[] array, int startIndexInclusive, int endIndexExclusive) {
 		return ArrayUtils.subarray(array, startIndexInclusive, endIndexExclusive);
 	}
